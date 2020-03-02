8aeff597c5bedc3f7cfef45e569dc8eefa587950
PhilRSmith <theonepudgypanda@gmail.com>
2020-03-02 15:57:50 -0500

    added dynamic cards to the left

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import SearchPage from './SearchPage';
 import * as serviceWorker from './serviceWorker';
 
 
 
-ReactDOM.render((<BrowserRouter> <App/> </BrowserRouter>), document.getElementById('root'));
+ReactDOM.render((<BrowserRouter> <SearchPage/> </BrowserRouter>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
