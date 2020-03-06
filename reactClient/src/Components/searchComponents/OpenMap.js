c7fde956bcd68f7551fec0f23658d04e5093266b
PhilRSmith <theonepudgypanda@gmail.com>
2020-03-06 08:57:38 -0500

    fixed modal on searchpage, editing map positions

--- reactClient/src/Components/searchComponents/OpenMap.js
+++ reactClient/src/Components/searchComponents/OpenMap.js
@@ -8,23 +8,24 @@ class OpenMap extends React.Component {
     constructor(props) {
        super(props);
        this.state = {
-       position: [51.505, -0.09],
+       position: [42.686063, -73.824688],
        }
    };
 
     render(){
       const mystyle = {
-        height: "800px",
-        width: "1200px"
+        height: "100%",
+        width: "100%"
       };
         return(
-          <div className="container-left" style={mystyle}>
+          <div className="container-left" >
             <Map center={this.state.position} zoom={13} style={mystyle}>
                 <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-                <Marker position={this.state.position}> {/*placeholder position until click functionality added*/}
+                <Marker position={this.state.position}>
+                <popup /> {/*placeholder position until click functionality added*/}
                 </Marker>
             </Map>
             </div>
