613ac15e1d85f1b34b63bdf17896da433c0c6065
Michael Sollazzo <msollazzo08@gmail.com>
2020-03-18 21:56:29 -0400

    add sidebar to dash

--- reactClient/src/Components/dashboard/sidebar.js
+++ reactClient/src/Components/dashboard/sidebar.js
@@ -1,57 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ProfileImgPlaceholder from '../../img/placeholder-profile-male-500x500.png';
-import '../dashboard/sidebar.css';
- class sidebar extends Component {
- 
-  render() {
+import './sidebar.css';
 
+ class sidebar extends Component {
+
+  render() {
     return (
     
-
-   <div className="custom" style={{backgroundImage: "linear-gradient(to bottom, rgba(200,0,0,0), rgba(86,77,128,1))"}}>
-     <div className="custom-container mt-4 border rounded" style={{backgroundColor: "#fafafa"}}>
-
-     <div className="row container-fluid pt-4 pl-4 justify-content-center" >
-      <div className="col-sm-3 card pt-2 ">
-        <img className="card-img-top" src={ProfileImgPlaceholder} height="190"  alt="Profile Image" />
-        <div className="card-body">
-         <div className="card-title">Michael Sollazzo</div>
-         <hr />
-         <div className="card-title">Status: Senior</div>
-         <hr />
-         <div className="card-title">Quad: State Quad</div>
+      <div className="sidebar d-flex flex-column">
+        <div className="mt-4">
+          <div className="text-center">
+            <img src={ProfileImgPlaceholder} className=" bg-dark rounded-circle border border-3 " alt="profile image" height="" width="120px" />
+          </div>
+          
+            <div className="text-light text-left px-3 py-2">
+                <h6 className="text-center">Donald Trump</h6>
+                <hr />
+                <p className="mb-0">Senior</p>
+                <p className="mb-0">Dutch Quad</p>
+                <hr />
+                <button className="btn btn-dark">Settings</button>
+            </div>
+            
         </div>
-      </div>
-      {/*************************************/}
-
-      <div className="h-100 col-sm-9 mt-3">       
-        <div className="table table-striped border rounded">
-          <thead>
-            <tr>
-              <th>User Data</th>
-            </tr>
-          </thead>
-          <tbody>
-                <tr>
-                  <td style={{width: "10%", background: "#97A1C3"}}>Posts:</td>
-                </tr>
-                <tr>
-                  <td>Tags:</td>
-                </tr>
-                <tr>
-                  <td style={{background: "#97A1C3"}}>Reviews:</td>
-                </tr>    
-          </tbody>
-        </div>
-      </div>
-     </div> 
-     </div>
-   </div>
-    
-      
-
+      </div> 
     )
   }
 }
+
 
 export default sidebar;
