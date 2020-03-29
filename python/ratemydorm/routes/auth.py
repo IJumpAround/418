7cb6514af6e768cfa4b713bdae6a8e590f231bfd
Nick <ventrn09@gmail.com>
2020-03-28 23:46:41 -0400

    fix login_required wrapper return type

--- python/ratemydorm/routes/auth.py
+++ python/ratemydorm/routes/auth.py
@@ -183,7 +183,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return 401
+            return "not authorized", 401
 
         return view(**kwargs)
     return wrapped_view
