300be7b3b3c25c46ca9d669a597cfd3f93d0d640
PhilRSmith <theonepudgypanda@gmail.com>
2020-03-28 22:44:29 -0400

    proximity filtering query added to searchpage

--- python/ratemydorm/routes/auth.py
+++ python/ratemydorm/routes/auth.py
@@ -11,7 +11,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     connection = get_connection()
-
+    cursor = connection.cursor()
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
