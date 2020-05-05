58392f3f15fc01cb47a3932e02b54a07b4262e8f
sollazzo <msollazzo08@gmail.com>
2020-05-05 14:57:40 -0400

    should work..

--- reactClient/src/Components/singleDorm/ReviewSection/reviewList.js
+++ reactClient/src/Components/singleDorm/ReviewSection/reviewList.js
@@ -4,13 +4,13 @@ import Review from './review'
  //list all of the reviews into an array
  function ReviewList(props) {
     console.log(props);
- const {user_id} = props.user_id;
+
  
   return (
     <div className="col-lg-12">
       
       {props.reviews.map((review, index)=> (
-        <Review  key={review} review={review} image={user_id}/>
+        <Review  key={review} review={review} />
       ))}
       
     </div>
