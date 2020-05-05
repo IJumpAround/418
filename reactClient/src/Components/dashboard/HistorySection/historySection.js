8b5562f5e3730e9bbcd179abf93e4073909f04c9
sollazzo <msollazzo08@gmail.com>
2020-05-05 08:50:59 -0400

    new container for user history section

--- reactClient/src/Components/dashboard/HistorySection/historySection.js
+++ reactClient/src/Components/dashboard/HistorySection/historySection.js
@@ -5,31 +5,31 @@ function HistorySection(props) {
   var reviews = props.reviews;
   const renderHistory = reviews.map((obj, index) => {    
     return(
-        <tr key={reviews[index].review_id}>
-          <td>Left a Review</td>
-          <td>{reviews[index].review_text}</td>
-          <td>{reviews[index].timestamp}</td>
-        </tr>  
+      
+          <li key={reviews[index].review_id} className="list-group-item">
+            <div className="row">
+            <div className="col-sm-4">
+               <tr>Left a Review</tr>
+            </div>
+            <div className="col-sm-4">
+              <tr>{reviews[index].review_text}</tr>
+            </div>
+            <div className="col-sm-4">
+              <tr>{reviews[index].timestamp}</tr>
+            </div>
+            </div>
+          </li>
+         
     )
   }
 );
     return (
-      <div className="table table-striped border rounded">
-      <div className="custom-thead">
-        <table>
-          <tbody>
-            <tr>
-              <th>History</th>     
-            </tr>
-          </tbody>
-        </table>
-      </div>
-     <table>
-      <tbody style={{width:"100%"}}>
-        {renderHistory}  
-      </tbody>
-     </table> 
-    </div>
+      <div>
+        <h3>User History</h3>
+        <div className="list-group scroll">
+          {renderHistory}
+       </div>
+      </div> 
     )
 }
 
